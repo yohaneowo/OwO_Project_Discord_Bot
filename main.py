b8e585ba431c793d3a7353f008fef2f3671680de
Yohanewww <yohane910@gmail.com>
2021-03-25 11:34:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("Logged in as {0.user}".format(client))
+
+@client.event
+async def on_message():
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('ggg')
+
+
+client.run(os.getenv('TOKEN'))
